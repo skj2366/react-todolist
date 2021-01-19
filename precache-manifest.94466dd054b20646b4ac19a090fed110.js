a6d9e3f1b1d16857f3b71d66256ff6b1203aa650
김동근 <skj2366@naver.com>
2021-01-19 22:25:55 +0900

    Updates

---
+++ precache-manifest.94466dd054b20646b4ac19a090fed110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a907acc4649c43b638e9b353fd34bc",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "0afbf84085cbd41773e0",
+    "url": "/react-todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4810cb0a5bc161f59b34",
+    "url": "/react-todolist/static/js/2.19853232.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist/static/js/2.19853232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afbf84085cbd41773e0",
+    "url": "/react-todolist/static/js/main.0b605a44.chunk.js"
+  },
+  {
+    "revision": "ad34c679752f7ee4e37c",
+    "url": "/react-todolist/static/js/runtime-main.4e5509e8.js"
+  }
+]);
